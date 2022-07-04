ae9748ae373b009c9400fe3fcbb08bebee14b8ed
oyesa <mercyoyesa2018@gmail.com>
2022-07-04 21:02:57 +0300

    create navbar component and add details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
