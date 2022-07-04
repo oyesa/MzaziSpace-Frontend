96a8e71cd08b67800fc919563a48d41a0fe35a0b
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-07-04 21:38:00 +0300

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
