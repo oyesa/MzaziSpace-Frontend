b17dd11c2a42d4f49d60462e3929f560badc543b
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-07-05 19:25:25 +0300

    Style and position homepage containers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,9 @@ import { LandingComponent } from './components/landing/landing.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutComponent } from './components/about/about.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 import { OurservicesComponent } from './components/ourservices/ourservices.component';
-import { CommunityComponent } from './components/community/community.component';
-import { GroupsComponent } from './components/groups/groups.component';
-import { MzazieventsComponent } from './components/mzazievents/mzazievents.component';
-import { TestimonialsComponent } from './components/testimonials/testimonials.component';
-import { SignupComponent } from './components/signup/signup.component';
+
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
@@ -19,7 +14,9 @@ const routes: Routes = [
   {path: 'home', component: HomepageComponent},
   {path: 'about', component: AboutComponent},
   {path: 'service', component: OurservicesComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+
 ];
 
 @NgModule({
