416296b159ca70ef21f88f7e6228adb1095ee55a
oyesa <mercyoyesa2018@gmail.com>
2022-07-05 13:13:59 +0300

    create other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
-import { NavbarComponent } from './components/navbar.component';
-import { FooterComponent } from './components/footer.component';
-import { OurservicesComponent } from './components/ourservices.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { OurservicesComponent } from './components/ourservices/ourservices.component';
+import { CommunityComponent } from './components/community/community.component';
+import { GroupsComponent } from './components/groups/groups.component';
+import { MzazieventsComponent } from './components/mzazievents/mzazievents.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { OurservicesComponent } from './components/ourservices.component';
     AboutComponent,
     NavbarComponent,
     FooterComponent,
-    OurservicesComponent
+    OurservicesComponent,
+    CommunityComponent,
+    GroupsComponent,
+    MzazieventsComponent,
+    TestimonialsComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
