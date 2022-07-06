3653c2ec1e6d45eaacedaa975738a9c2cf449c2e
oyesa <mercyoyesa2018@gmail.com>
2022-07-06 12:34:10 +0300

    create find a counselor page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MzazieventsComponent } from './components/mzazievents/mzazievents.compo
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { FindcounselorComponent } from './components/findcounselor/findcounselor.component';
 const appRoutes: Routes = [
   
 ]
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
     MzazieventsComponent,
     TestimonialsComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    FindcounselorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/findcounselor/findcounselor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-findcounselor',
+  templateUrl: './findcounselor.component.html',
+  styleUrls: ['./findcounselor.component.css']
+})
+export class FindcounselorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
