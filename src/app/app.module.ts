8946515cd56b2d5361e2588b7c90f23fe0425f4a
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-07-11 06:21:35 +0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes} from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './components/landing/landing.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutComponent } from './components/about/about.component';
@@ -18,23 +16,17 @@ import { MzazieventsComponent } from './components/mzazievents/mzazievents.compo
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
-import { FindcounselorComponent } from './components/findcounselor/findcounselor.component';
-import { ChildservicesComponent } from './components/childservices/childservices.component';
-import { SinglefathersComponent } from './components/singlefathers/singlefathers.component';
-import { SpecialComponent } from './components/special/special.component';
-import { WellbeingComponent } from './components/wellbeing/wellbeing.component';
-import { UpdateProfileComponent } from './components/profile/update-profile/update-profile.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
 const appRoutes: Routes = [
-  
+  {path: 'home', component: HomepageComponent},
+  {path: 'community', component: CommunityComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'service', component: OurservicesComponent},
+  {path: 'testimonials', component: TestimonialsComponent},
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
     ProfileComponent,
     HomepageComponent,
     AboutComponent,
@@ -46,22 +38,12 @@ const appRoutes: Routes = [
     MzazieventsComponent,
     TestimonialsComponent,
     SignupComponent,
-    LoginComponent,
-    FindcounselorComponent,
-    ChildservicesComponent,
-    SinglefathersComponent,
-    SpecialComponent,
-    WellbeingComponent,
-    UpdateProfileComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes,{enableTracing: true}),
-    HttpClientModule,
-    NgbModule,
-    FormsModule,
-    ReactiveFormsModule
+    RouterModule.forRoot(appRoutes,{enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
