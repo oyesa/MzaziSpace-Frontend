12f2495136a1ba2b57a9df63a10f2daee972d07d
oyesa <mercyoyesa2018@gmail.com>
2022-07-13 21:56:01 +0300

    update childservice file

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,13 +1,12 @@
 
-
- const sign_in_btn = document.querySelector("#sign-in-btn");
- const sign_up_btn = document.querySelector("#sign-up-btn");
+ const signInBtn = document.getElementById('#signInBtn');
+ const signUpBtn = document.getElementById("#signUpBtn");
  const container = document.querySelector(".container");
  
- sign_up_btn.addEventListener("click", () => {
- container.classList.add("sign-up-mode");
+ signUpBtn.addEventListener("click", () => {
+ container.classList.add("signUpBtn");
  });
  
- sign_in_btn.addEventListener("click", () => {
- container.classList.remove("sign-up-mode");
+ signInBtn.addEventListener("click", () => {
+ container.classList.remove("signUpBtn");
  });
