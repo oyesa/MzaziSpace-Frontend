62c43128953669bb0a8f6a97bf48b9358613d40b
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-07-14 12:46:08 +0300

    Add Styling of modal in global styling sheet

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -11,8 +11,7 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  inboundClick = true;
-  outboundClick = true;
+  
   profile: any
   profileForm: FormGroup
   showModal = false
@@ -23,7 +22,9 @@ export class ProfileComponent implements OnInit {
   constructor( private authService:AuthServiceService, private fb: FormBuilder, private modalService: NgbModal ) { }
 
   open(content:any) {
+    
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
